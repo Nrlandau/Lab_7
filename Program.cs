99816576fc380987076996453575e4534988e8bf
Nicholas Landau <nrlandau@gmail.com>
2018-10-15 14:41:59 -0400

    Created IsPhoneNumber

--- Program.cs
+++ Program.cs
@@ -13,11 +13,16 @@ namespace Lab_7
         {
             return Regex.IsMatch(_email, @"^[\w\d]{5,30}@[\w\d]{5,10}\.[\w\d]{2,3}$");
         }
-
+        static bool IsPhoneNumber(string _Phone)
+        {
+            return Regex.IsMatch(_Phone, @"\d{3}-\d{3}-\d{4}");
+        }
         static void Main(string[] args)
         {   
             System.Console.WriteLine(IsName("Hello World"));
             System.Console.WriteLine(IsEmail("asdfasdf@asdfa.co"));
+            System.Console.WriteLine(IsPhoneNumber("999-999-9999"));
+
         }
     }
 }
