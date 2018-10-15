160b0064813bbbcbbaffc825bf4e1bce4bf29644
Nicholas Landau <nrlandau@gmail.com>
2018-10-15 14:58:29 -0400

    Made Sure Day Is Valid

--- Program.cs
+++ Program.cs
@@ -19,7 +19,34 @@ namespace Lab_7
         }
         static bool IsDate(string _Date)    //dd/mm/yyyy
         {
-            return (Regex.IsMatch(_Date, @"^[0123]\d/[01]\d/\d{4}$"));
+            int day;
+            if (Regex.IsMatch(_Date, @"^[0123]\d/(0\d|1[012])/\d{4}$"))
+            {
+                day = int.Parse(_Date.Substring(0,2));
+                if( day != 0 && day <= GetDaysInMonth(int.Parse(_Date.Substring(3,2)),int.Parse(_Date.Substring(6,4))))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        static int GetDaysInMonth(int _Month, int _Year)
+        {
+            switch(_Month)
+            {
+                case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                    return 31;
+                case 4: case 6: case 9: case 11:
+                    return 30;
+                case 2:
+                    return IsLeapYear() ? 29 : 28;
+                default:
+                    return -1;
+            }
+        }
+        static bool IsLeapYear()
+        {
+            return false;
         }
         static void Main(string[] args)
         {   
@@ -27,6 +54,7 @@ namespace Lab_7
             System.Console.WriteLine(IsEmail("asdfasdf@asdfa.co"));
             System.Console.WriteLine(IsPhoneNumber("999-999-9999"));
             System.Console.WriteLine(IsDate("12/12/1234"));
+            System.Console.WriteLine(IsDate("34/12/1234"));
         }
     }
 }
