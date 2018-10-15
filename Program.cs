ec50a11f3bff673c938de177b4b27c64feae9af6
Nicholas Landau <nrlandau@gmail.com>
2018-10-15 14:39:10 -0400

    Created IsEmail

--- Program.cs
+++ Program.cs
@@ -5,13 +5,19 @@ namespace Lab_7
 {
     class Program
     {
-        static bool IsName(string name)
+        static bool IsName(string _name)
         {
-            return Regex.IsMatch(name, @"^[A-Z][\w ]{0,29}$");
+            return Regex.IsMatch(_name, @"^[A-Z][\w ]{0,29}$");
         }
+        static bool IsEmail(string _email)
+        {
+            return Regex.IsMatch(_email, @"^[\w\d]{5,30}@[\w\d]{5,10}\.[\w\d]{2,3}$");
+        }
+
         static void Main(string[] args)
         {   
             System.Console.WriteLine(IsName("Hello World"));
+            System.Console.WriteLine(IsEmail("asdfasdf@asdfa.co"));
         }
     }
 }
