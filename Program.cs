fe3c344110988632cbe3863c7bd1e0b0ca3fbbec
Nicholas Landau <nrlandau@gmail.com>
2018-10-15 14:30:34 -0400

    Created Project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Lab_7
+{
+    class Program
+    {
+        static bool IsName()
+        {
+            return true;
+        }
+        static void Main(string[] args)
+        {   
+
+        }
+    }
+}
